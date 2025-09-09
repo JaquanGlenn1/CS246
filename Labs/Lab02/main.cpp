caf147caf8b92b50db35f481e466115705316327
JaquanGlenn1 <jaquang100@gmail.com>
2025-09-08 20:31:04 -0400

    Update main.cpp

--- Labs/Lab02/main.cpp
+++ Labs/Lab02/main.cpp
@@ -40,3 +40,130 @@ int main() {
 
 // task 3
 
+#include <iostream>
+using namespace std;
+
+int *getScores(int &size) {
+  cout << "Enter number of scores: ";
+  cin >> size;
+
+  while (size <= 0) {
+    cout << "Enter a positive number: ";
+    cin >> size;
+  }
+
+  int *scores = new int[size];
+
+  for (int i = 0; i < size; i++) {
+    cout << "Enter score " << i + 1 << ": ";
+    cin >> scores[i];
+  }
+
+  return scores;
+}
+
+int main() {
+  int size;
+  int *scores = getScores(size);
+
+  cout << "Scores are: ";
+  for (int i = 0; i < size; i++) {
+    cout << scores[i] << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
+
+//task 4
+
+#include <iostream>
+using namespace std;
+
+int* Join(int a1[], int n1, int a2[], int n2) {
+    int* a = new int[n1 + n2];
+
+    for (int i = 0; i < n1; i++) {
+        a[i] = a1[i];
+    }
+    for (int i = 0; i < n2; i++) {
+        a[n1 + i] = a2[i];
+    }
+
+    return a;
+}
+
+int main() {
+    int arr1[3] = {1, 2, 3};
+    int arr2[4] = {4, 5, 6, 7};
+
+    int* joined = Join(arr1, 3, arr2, 4);
+
+    cout << "Joined: ";
+    for (int i = 0; i < 7; i++) {
+        cout << joined[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+// task 5
+
+#include <iostream>
+using namespace std;
+
+void PointerExample() {
+    int x = 10;
+    int* p = &x;
+    cout << "PointerExample: x = " << x << ", *p = " << *p << endl;
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int* getScores(int& size) {
+    cout << "Enter number of scores: ";
+    cin >> size;
+    while (size <= 0) {
+        cout << "Enter positive number: ";
+        cin >> size;
+    }
+
+    int* a = new int[size];
+    for (int i = 0; i < size; i++) {
+        cout << "Enter score " << i+1 << ": ";
+        cin >> a[i];
+    }
+    return a;
+}
+
+int* Join(int a1[], int n1, int a2[], int n2) {
+    int* a = new int[n1+n2];
+    for (int i = 0; i < n1; i++) a[i] = a1[i];
+    for (int i = 0; i < n2; i++) a[n1+i] = a2[i];
+    return a;
+}
+
+int main() {
+    PointerExample();
+
+    int size;
+    int* scores = getScores(size);
+    cout << "Scores: ";
+    printArray(scores, size);
+
+    int arr1[3] = {1,2,3};
+    int arr2[4] = {4,5,6,7};
+    int* joined = Join(arr1,3,arr2,4);
+    cout << "Joined array: ";
+    printArray(joined,7);
+
+    delete[] scores;
+    delete[] joined;
+
+    return 0;
+}
